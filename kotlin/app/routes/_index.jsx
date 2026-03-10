0a057bf333c6d2b80cfd9c3d406960f5072e7ce2
mateusz.nitkowski@gmail.com <Jungkookopp@123>
2026-03-10 19:23:33 +0100

    website title and finished website

--- kotlin/app/routes/_index.jsx
+++ kotlin/app/routes/_index.jsx
@@ -1,6 +1,10 @@
 import { OverviewPage } from "../pages/index/index.jsx";
 
-
+export function meta({}) {
+  return [
+    { title: "Kotlin Programming Language" },
+  ];
+}
 
 export default function Index() {
   return (
